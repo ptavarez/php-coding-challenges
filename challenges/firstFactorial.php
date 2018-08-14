6c4e8f110085585678adbf111cfc392b3520d48b
ptavarez16 <ptavarez16@yahoo.com>
2018-08-14 12:45:13 -0400

    Add testing for first factorial challenge

--- challenges/firstFactorial.php
+++ challenges/firstFactorial.php
@@ -8,7 +8,13 @@ integer.
     // Code here
   }
   
-  echo firstFactorial(4) // => 24
-  echo firstFactorial(8) // => 40320
-  echo firstFactorial(12) // => 479001600
+  if (firstFactorial(4) == 24 &&
+      firstFactorial(8) == 40320 &&
+      firstFactorial(12) == 479001600) {
+    echo 'Factorial of 4 = '.firstFactorial(4).' | ';
+    echo 'Factorial of 8 = '.firstFactorial(8).' | ';
+    echo 'Factorial of 12 = '.firstFactorial(12).' | ';
+  } else {
+    echo 'Err, try again! ';
+  }
 ?>
