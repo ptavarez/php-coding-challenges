77e3bae025115f07e16b3464476dabff5f6b4061
ptavarez16 <ptavarez16@yahoo.com>
2018-08-14 12:26:02 -0400

    Add first factorial challenge

--- challenges/firstFactorial.php
+++ challenges/firstFactorial.php
@@ -1,4 +1,4 @@
-Have the function firstFactorial($num) take the $num parameter being passed and
+Have the function firstFactorial($num) take the $finum parameter being passed and
 return the factorial of it (e.g. if $num = 4, return (4 * 3 * 2 * 1)). For the
 test cases, the range will be between 1 and 18 and the input will always be an
 integer.
